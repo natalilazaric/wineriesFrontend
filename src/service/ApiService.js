7a1c3ba0994cbcf179e4f7b0811ffe93c8fce79c
natalilazaric <nl53743@fer.hr>
2025-04-20 17:04:50 +0200

    Updated backend URL to Render

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default class ApiService{
 
-    static BASE_URL = "http://localhost:4040"
+    static BASE_URL = "https://wineries-backend.onrender.com"
 
     static getHeader(){
         const token = localStorage.getItem("token");
